ba848156d028c6e9c73fea0d2a619ee4f096b369
Djordje Lipovcic <the2922000@gmail.com>
2021-11-14 18:45:53 +0100

    Napravljen MainFrame

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,35 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+
+	public MainFrame() {
+
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		
+		setSize(screenWidth * 3 / 4, screenHeight * 3 / 4);
+		
+		setTitle("Studentska sluzba");
+	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLocationRelativeTo(null);
+		
+		
+		/*MenuBar menu = new MenuBar();
+		this.setJMenuBar(menu);
+		
+		ToolBar toolbar = new ToolBar();
+		add(toolbar,BorderLayout.NORTH);*/
+	
+	}
+}
---
+++ src/project/Main.java
@@ -0,0 +1,13 @@
+package project;
+
+import gui.MainFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainFrame studentskaSluzba = new MainFrame();
+		studentskaSluzba.setVisible(true);
+	}
+
+}
